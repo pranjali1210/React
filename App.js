bc8bc875dc5e316a653c886f92f70ed4bc92a0e5
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-22 20:49:07 +0530

    5. Functional Component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {User1} from './User1'
 function App() {
   return (
     <div className="App">
       <h1> Hello World</h1>
+      <User1 />
     </div>
   );
 }
