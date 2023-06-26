cde9d0682db42dc46e74f6f96d74aa015de9db8c
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-26 16:43:16 +0530

    19. Basic form Validation

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Profile from './Profile';
+import Login from './Login';
 function App()
 {
   return(
     <div className='App'>
-      <Profile />
+      <Login />
     </div>
   )
 }
