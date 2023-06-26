c4865cbda0051a6af819816a844aad393f4952b5
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-26 15:54:50 +0530

    18. Conditional Rendering | If Condition

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState} from 'react';
+import Profile from './Profile';
 function App()
 {
-  const [name,setName]=useState("")
-  const [tnc,setTnc]=useState(false)
-  const [interest,setInterest]=useState("")
-  function getFormData(e)
-  {
-    console.warn(name,tnc,interest)
-    e.preventDefault()
-  }
   return(
     <div className='App'>
-      <h1>Handle form in React</h1>
-      <form onSubmit={getFormData}>
-        <input type="text" placeholder='Enter Name' onChange={(e)=>setName(e.target.value)}/><br></br>
-        <br></br>
-        <select onChange={(e)=>setInterest(e.target.value)}>
-          <option>Select Option</option>
-          <option>Marvel</option>
-          <option>DC</option>
-        </select>
-        <br></br>
-        <br></br>
-        <br></br>
-        <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)}/><span>Accept Terms And Conditions</span>
-          <br></br><br></br>
-          <button type="submit" >Submit</button>
-          <button type="reset">Clear</button>
-        </form>
+      <Profile />
     </div>
   )
 }
