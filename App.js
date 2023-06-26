089a0880efecdcb66d9f16624f71a838d4c5de39
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-26 13:42:15 +0530

    14. props in class component

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Student from './Student';
-import React, {useState} from 'react';
-function App()
-{
-  const [name,setName]=useState("Pranjali")
-  return(
-    <div className='App'>
-      <Student name={name} />
-      <button onClick={()=>{setName("Asha")}}>Update Name</button>
-       
-    </div>
-    
-  )
+import Student1 from './Student1'
+import React from 'react'
+class App extends React.Component {
+  constructor()
+  {
+    super();
+    this.state={
+      name:"Anil"
+    }
+  }
+  render()
+  {
+    return (
+      <div className="App">
+       <h1>Props !</h1>
+       <Student1 name={this.state.name} email="anil@test.com"></Student1>
+       <button onClick={()=>this.setState({name:"Sidhu"})} >Update Name</button>
+      </div>
+    );
+  }
 }
-
 
 export default App;
