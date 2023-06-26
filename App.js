cef14a38604c41dc536685b1c103ccf846f9a9c6
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-26 13:59:51 +0530

    15. Get Input Box Value

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import Student1 from './Student1'
-import React from 'react'
-class App extends React.Component {
-  constructor()
+import React,{useState} from 'react';
+function App()
+{
+  const [data,setData]=useState(null)
+  const [print,setPrint]=useState(false)
+  function getData(val)
   {
-    super();
-    this.state={
-      name:"Anil"
-    }
+    console.warn(val.target.value)
+    setData(val.target.value)
+    setPrint(false)
   }
-  render()
-  {
-    return (
-      <div className="App">
-       <h1>Props !</h1>
-       <Student1 name={this.state.name} email="anil@test.com"></Student1>
-       <button onClick={()=>this.setState({name:"Sidhu"})} >Update Name</button>
-      </div>
-    );
-  }
+  return(
+    <div className='App'>
+      {
+        print?
+        <h1>{data}</h1>
+        :null
+      }
+      
+      <input type="text" onChange={getData}/>
+      <button onClick={()=>setPrint(true)}>Print Data</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
