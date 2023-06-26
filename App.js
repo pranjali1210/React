d3de2aa7eb425d2cdc403fca11cfeaa8b82d217f
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-26 12:38:50 +0530

    Updated 13 . props with functional component

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Student from './Student';
-
+import React, {useState} from 'react';
 function App()
 {
+  const [name,setName]=useState("Pranjali")
   return(
     <div className='App'>
-      <Student name={"Pranjali"} email="pranjali@gmail.com"/>
-      <Student name={"Asha"} email="asha@gmail.com"/>
+      <Student name={name} />
+      <button onClick={()=>{setName("Asha")}}>Update Name</button>
+       
     </div>
     
   )
--- Student.js
+++ Student.js
@@ -3,7 +3,7 @@ function Student(p)
     return(
         <div style={{backgroundColor:"skyblue", margin:10}}>
             <h1>Hello {p.name}</h1>
-            <h2>Email : {p.email}</h2>
+            
         </div>
 
     )
