9efcafdcb83356c6860915f82d464a8ce0739c40
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-27 13:49:39 +0530

    29. useEffect Hook in ReactJS

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState} from 'react';
+import React,{useEffect,useState} from 'react';
 function App()
 {
-  const [data,setData]=useState("Asha")
+  const [count,setCount]=useState(0)
+  useEffect(()=>{
+
+  })
   return(
     <div className='App'>
-      <h1>Hello {data}</h1>
-      <button onClick={()=>setData("Pranjali")}>Update Name</button>
+      <h1>useEffect in ReactJS {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Update Counter</button>
     </div>
   )
 }
