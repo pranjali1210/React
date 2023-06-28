fcceabb60742ddeaae6c25a3f67afda9c7aeac17
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-28 13:19:10 +0530

    34. List with Bootstrap Table

--- App.js
+++ App.js
@@ -1,33 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Table } from 'react-bootstrap';
 function App() {
   const students=[
-    {name:"Pranjali",email:"pranju@gmail.com",phone:8745343563},
-    {name:"Asha",email:"asha@gmail.com",phone:955743563},
-    {name:"Sujal",email:"sujal@gmail.com",phone:765343563}
+    {name:"Pranjali",email:"pranju@gmail.com",phone:222},
+    {name:"Asha",email:"asha@gmail.com",phone:111},
+    {name:"Sujal",email:"sujal@gmail.com",phone:777},
+    {name:"Soham",email:"soham@gmail.com",phone:111}
   ]
   return (
     <div className='App'>
-      <h1>Handle Array with List</h1>
-      <table border="1">
-      <tr>
-            <td>name</td>
-            <td>email</td>
-            <td>phone</td>
-          </tr>
+      <h1>List with Bootstrap Table</h1>
+      <Table striped bordered hover variant="dark">
+        <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Contact</td>
+        </tr>
+        
       {
-        
-        students.map((i) =>
-          <tr>
-            <td>{i.name}</td>
-            <td>{i.email}</td>
-            <td>{i.phone}</td>
-          </tr>
+        students.map((item,i)=>
+        item.phone===111?
+        <tr key={i}>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+          <td>{item.phone}</td>
+        </tr>:null
         )
-        
       }
-      </table>
-
+      </tbody>
+      </Table>
     </div>
   )
 }
