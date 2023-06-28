ef53f80c37f12d5b3ca8f588de455275e947ce97
Pranjali Shinde <134287860+pranjali1210@users.noreply.github.com>
2023-06-28 13:57:15 +0530

    35. Nested List in ReactJS

--- App.js
+++ App.js
@@ -3,35 +3,83 @@ import './App.css';
 import React from 'react';
 import { Table } from 'react-bootstrap';
 function App() {
-  const students=[
-    {name:"Pranjali",email:"pranju@gmail.com",phone:222},
-    {name:"Asha",email:"asha@gmail.com",phone:111},
-    {name:"Sujal",email:"sujal@gmail.com",phone:777},
-    {name:"Soham",email:"soham@gmail.com",phone:111}
+  const students = [
+    {
+      name: "Pranjali", email: "pranju@gmail.com", address: [
+        { hn: "10", city: "Noida", country: "India" },
+        { hn: "34", city: "Gurgaon", country: "India" },
+        { hn: "43", city: "Delhi", country: "India" },
+        { hn: "90", city: "Noida", country: "India" }
+      ]
+    },
+    {
+      name: "Asha", email: "asha@gmail.com", address: [
+        { hn: "10", city: "Noida", country: "India" },
+        { hn: "34", city: "Gurgaon", country: "India" },
+        { hn: "43", city: "Delhi", country: "India" },
+        { hn: "90", city: "Noida", country: "India" }
+      ]
+    },
+    {
+      name: "Sujal", email: "sujal@gmail.com", address: [
+        { hn: "10", city: "Noida", country: "India" },
+        { hn: "34", city: "Gurgaon", country: "India" },
+        { hn: "43", city: "Delhi", country: "India" },
+        { hn: "90", city: "Noida", country: "India" }
+      ]
+    },
+    {
+      name: "Soham", email: "soham@gmail.com", address: [
+        { hn: "10", city: "Noida", country: "India" },
+        { hn: "34", city: "Gurgaon", country: "India" },
+        { hn: "43", city: "Delhi", country: "India" },
+        { hn: "90", city: "Noida", country: "India" }
+      ]
+    },
   ]
   return (
     <div className='App'>
       <h1>List with Bootstrap Table</h1>
-      <Table striped bordered hover variant="dark">
+      <Table variant='dark' striped>
         <tbody>
-        <tr>
-          <td>Name</td>
-          <td>Email</td>
-          <td>Contact</td>
-        </tr>
-        
-      {
-        students.map((item,i)=>
-        item.phone===111?
-        <tr key={i}>
-          <td>{item.name}</td>
-          <td>{item.email}</td>
-          <td>{item.phone}</td>
-        </tr>:null
-        )
-      }
-      </tbody>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Address</td>
+          </tr>
+
+          {
+            students.map((item) =>
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>
+                  <Table variant='dark' striped>
+                  <tr>
+                      <td>House No</td>
+                      <td>City</td>
+                      <td>Contact</td>
+                     </tr>
+                    {
+                      
+                      item.address.map((data) =>
+
+                        <tr>
+                          <td>{data.hn}</td>
+                          <td>{data.city}</td>
+                          <td>{data.country}</td>
+                        </tr>
+                      )
+                    
+                      }
+                      </Table>
+                </td>
+              </tr>
+            )
+          }
+        </tbody>
       </Table>
+
     </div>
   )
 }
