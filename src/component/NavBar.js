95aae8053c4d4bef8654f2affb13b7f1b515a2b2
pranjali1210 <roll.rbtl20it176@gmail.com>
2023-07-02 20:01:42 +0530

    Link and NavLink in Reactjs

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,14 +1,14 @@
-import {Link} from 'react-router-dom'
+import {Link,NavLink} from 'react-router-dom'
 function NavBar()
 {
     return(
         <div>
-        <ul>
+        <ul className='navbar'>
             {/*<li></li>
             //<li><Link to="/home">Home</Link></li> */}
-            <li><Link to="/home">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><NavLink className="navbarLink" to="/home">Home</NavLink></li>
+            <li><NavLink className="navbarLink" to="/about">About</NavLink></li>
+            <li><NavLink className="navbarLink" to="/contact">Contact</NavLink></li>
             
         </ul>
         </div>
