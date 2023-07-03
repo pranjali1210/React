32e85b60b25c172a65da3354621b09487e736d0b
pranjali1210 <roll.rbtl20it176@gmail.com>
2023-07-03 19:27:43 +0530

    useLocation Hook

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,19 +1,15 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useLocation} from "react-router-dom"
 function Home()
 {
-    const navigate=useNavigate()
-    const navTopage=(URL)=>{
-        navigate(URL)
-        
-    }
+    const location=useLocation()
+    console.log(location)
     return(
     <div>
         <h1>Home Page !!</h1>
         <p>This is Home Page of our awesome App</p>
         <p>And here we are learning about Router's</p>
         <Link to="/about">go to about page</Link><br />
-        <button onClick={()=> navTopage('/about')}>Go to About Page</button><br /><br />
-        <button onClick={()=> navTopage('/filter')}>Go to Filter Page</button>
+        
     </div>
     )
 }
