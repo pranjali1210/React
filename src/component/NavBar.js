2582ee07a8ebf95a68339e1c372741e7ff7c45d8
pranjali1210 <roll.rbtl20it176@gmail.com>
2023-07-03 13:28:04 +0530

    Active Link in ReactJS

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -6,9 +6,15 @@ function NavBar()
         <ul className='navbar'>
             {/*<li></li>
             //<li><Link to="/home">Home</Link></li> */}
-            <li><NavLink className="navbarLink" to="/home">Home</NavLink></li>
-            <li><NavLink className="navbarLink" to="/about">About</NavLink></li>
-            <li><NavLink className="navbarLink" to="/contact">Contact</NavLink></li>
+            <li><NavLink 
+            style={({isActive})=>{return {backgroundColor: isActive ?'green':'red'}}}
+            className="navbarLink" to="/home">Home</NavLink></li>
+            <li><NavLink 
+            style={({isActive})=>{return {backgroundColor: isActive ?'green':'red'}}}
+            className="navbarLink" to="/about">About</NavLink></li>
+            <li><NavLink 
+            style={({isActive})=>{return {backgroundColor: isActive ?'green':'red'}}}
+            className="navbarLink" to="/contact">Contact</NavLink></li>
             
         </ul>
         </div>
